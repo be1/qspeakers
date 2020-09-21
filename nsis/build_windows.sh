cda468ea169b9bc7d7407ebd6d0d1f4b73426d6e
Benoît Rouits <brouits@free.fr>
2020-09-22 00:26:31 +0200

    fix windows builder

--- nsis/build_windows.sh
+++ nsis/build_windows.sh
@@ -10,8 +10,9 @@ else
 	ARCH="$1" # i686 or x86_64
 fi
 
-PATH=/usr/lib/mxe/usr/bin
+PATH=/usr/lib/mxe/usr/bin:$PATH
 cd ..
+/usr/bin/make distclean
 /usr/lib/mxe/usr/bin/${ARCH}-w64-mingw32.static-qmake-qt5 -config release
 /usr/bin/make
 cd nsis
