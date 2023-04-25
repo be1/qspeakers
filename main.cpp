48d917a40c01cea35cb47c6910c6a90230dec364
Benoît Rouits <brouits@free.fr>
2023-04-25 20:46:48 +0200

    Remove org domain to fix desktop icon display.

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setOrganizationName("Herewe");
-    a.setOrganizationDomain("herewe");
+    //a.setOrganizationDomain("herewe");
     a.setApplicationName("QSpeakers");
     a.setApplicationVersion(VERSION " (" REVISION ")");
 
