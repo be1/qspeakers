5ff2e17cb1308fa5f8ebc4838e805e0297371358
Benoît Rouits <brouits@free.fr>
2026-09-04 21:49:58 +0200

    Prevent cross build if mxe or nsis are not present

--- nsis/build_windows.sh
+++ nsis/build_windows.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 #
 
+if ! [ -f /usr/bin/makensis ]; then
+	echo "makensis must be installed"
+	exit 1
+fi
+if ! [ -d $HOME/src/mxe/usr/bin ]; then
+	echo "mxe binaries must be installed under $HOME/src/mxe"
+	exit 1
+fi
+
 if [ "$1" = "" ]
 then
 	ARCH="x86_64"
